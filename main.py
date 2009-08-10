b4e830b83a62d7474efd99068ad6a79e6b75c84c
Paul Howe <pahowe@gmail.com>
2009-08-10 21:31:53 +0200

    Adding first set of files

---
+++ main.py
@@ -0,0 +1,24 @@
+
+import os
+
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+class MainPage(webapp.RequestHandler):
+  def get(self):
+    self.response.headers['Content-Type'] = 'text/html'
+
+    template_values = {}
+    path = os.path.join(os.path.dirname(__file__),'templates','index.html')
+    self.response.out.write(template.render(path, template_values))
+
+application = webapp.WSGIApplication(
+                                     [('/', MainPage)],
+                                     debug=True)
+
+def main():
+  run_wsgi_app(application)
+
+if __name__ == "__main__":
+  main()
